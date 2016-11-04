774eaeae66f8e4ac761c8df7715f5862b42e8a44
sunandan barman <sunandan.barman@gmail.com>
2016-11-04 01:02:07 -0400

    Update ChatHeadService.java

--- packages/apps/chatheads/src/com/example/chatheads/ChatHeadService.java
+++ packages/apps/chatheads/src/com/example/chatheads/ChatHeadService.java
@@ -10,16 +10,50 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.content.ComponentName;
-import android.widget.Toast;
-import android.view.View.OnClickListener;
 import android.util.Log;
+
+import android.content.pm.PackageManager;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.ResolveInfo;
+import android.os.Parcelable;
+
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
 
 public class ChatHeadService extends Service {
 
 	private WindowManager windowManager;
 	private ImageView chatHead;
 	WindowManager.LayoutParams params;
+	List<Intent> targetedShareIntents;
 
+	public void getListOfAllIntents() {
+	    Log.v("ChatHeadService","Content Chooser");
+		PackageManager pm = getPackageManager();
+		
+		//Create new Intent with specific action and category to get a list of applications
+		Intent intent = new Intent();
+		intent.setAction(Intent.ACTION_MAIN);
+		intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+		//Get a list of apps with the properties of intent created above
+		List<ResolveInfo> launchables=pm.queryIntentActivities(intent, 0);
+		Collections.sort(launchables,new ResolveInfo.DisplayNameComparator(pm));
+
+		targetedShareIntents = new ArrayList<Intent>();
+		
+		for (ResolveInfo launch : launchables) {
+			String packageName = launch.activityInfo.packageName;
+			Log.v("ChatHeadService","LaunchableAppPackageName:"+packageName);
+			Intent targetedShareIntent = new Intent();
+			targetedShareIntent.setPackage(packageName);
+			targetedShareIntent.putExtra("parentId",1);
+			//Add package to Intent Chooser list
+			targetedShareIntents.add(targetedShareIntent);
+		}
+
+	}
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -33,20 +67,15 @@ public class ChatHeadService extends Service {
 				WindowManager.LayoutParams.WRAP_CONTENT,
 				WindowManager.LayoutParams.WRAP_CONTENT,
 				WindowManager.LayoutParams.TYPE_PHONE,
-				WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM |
-				WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
 				PixelFormat.TRANSLUCENT);
 
 		params.gravity = Gravity.TOP | Gravity.LEFT;
 		params.x = 0;
 		params.y = 0;
 
-		chatHead.setOnClickListener(new View.OnClickListener() {
-		    @Override
-		    public void onClick(View v) {
-		        Log.v("chatHead","OnClickListener called");
-		    }
-		});
+		getListOfAllIntents();
+
 		//this code is for dragging the chat head
 		chatHead.setOnTouchListener(new View.OnTouchListener() {
 			private int initialX;
@@ -62,12 +91,8 @@ public class ChatHeadService extends Service {
 				case MotionEvent.ACTION_DOWN:
 					initialX = params.x;
 					initialY = params.y;
-
-
 					initialTouchX = event.getRawX();
 					initialTouchY = event.getRawY();
-					// Log.v("chatHeadTouch","DOWN_initialX " + initialX + "=" + initialY);
-					// Log.v("chatHeadTouch","DOWN_initialTouch " + event.getRawX() + "=" + event.getRawY());
 					return true;
 				case MotionEvent.ACTION_UP:
 					//Does not work
@@ -86,13 +111,20 @@ public class ChatHeadService extends Service {
 						|| Math.abs(finalTouchY-initialY) >= 1 ) {
 						return true;
 					}
-					final String OVERLAY_PACKAGE_NAME = "com.example.chatheads";
-    				final String OVERLAY_CLASS_NAME = "com.example.chatheads.OverlayActivity";
-         			Intent intent = new Intent();
-         			intent.setAction(Intent.ACTION_MAIN);
-         			intent.addCategory(Intent.CATEGORY_LAUNCHER);
-         			intent.setComponent(new ComponentName(OVERLAY_PACKAGE_NAME, OVERLAY_CLASS_NAME));
-         			startActivity(intent);
+
+
+            				Log.v("ChatHeadService", "TargetIntentSize"+ Integer.toString(targetedShareIntents.size()));
+					Intent chooserIntent = Intent.createChooser(targetedShareIntents.remove(0), "Select Overlay App");
+					chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetedShareIntents.toArray(new Parcelable[targetedShareIntents.size()]));
+					startActivity(chooserIntent);
+
+					// final String OVERLAY_PACKAGE_NAME = "com.example.chatheads";
+    	// 			final String OVERLAY_CLASS_NAME = "com.example.chatheads.OverlayActivity";
+     //     			Intent intent = new Intent();
+     //     			intent.setAction(Intent.ACTION_MAIN);
+     //     			intent.addCategory(Intent.CATEGORY_LAUNCHER);
+     //     			intent.setComponent(new ComponentName(OVERLAY_PACKAGE_NAME, OVERLAY_CLASS_NAME));
+     //     			startActivity(intent);
 
          			//Does not work
          			//Intent i = Intent.makeMainSelectorActivity(Intent.ACTION_MAIN, Intent.CATEGORY_APP_CALCULATOR);
@@ -112,17 +144,15 @@ public class ChatHeadService extends Service {
 				return false;
 			}
 		});
-		chatHead.bringToFront();
-		chatHead.setClickable(true); 
 		windowManager.addView(chatHead, params);
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		if (chatHead != null) 
+		if (chatHead != null)
 			windowManager.removeView(chatHead);
-		
+
 	}
 
 	@Override
